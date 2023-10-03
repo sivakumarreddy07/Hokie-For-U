c3674407a28d9cd5e579465af07fc6df6948a738
Akshay-06 <akshayreddy1999@gmail.com>
2023-10-03 16:14:28 -0400

    Basic React app template

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './css/App.css'
+
+function App() {
+  return (
+    <div className="App">
+      Basic React App Template
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/HeaderApp.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import '../css/HeaderApp.css';
+
+
+class HeaderApp extends Component {
+  render() {
+    return (
+      <div className='app-header'>
+        Header Component
+      </div>
+    )
+  }
+}
+
+export default HeaderApp
