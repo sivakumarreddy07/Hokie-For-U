f2c16c4d8f6a3fca6e6493f2455226b3e5048ea5
Akshay Narra <51908603+Akshay-06@users.noreply.github.com>
2023-12-08 13:34:45 -0500

    Create config.example.js

---
+++ server/config/config.example.js
@@ -0,0 +1,10 @@
+//This file holds any configuration variables we may need
+//'config.js' is usually ignored by git to protect sensitive information, such as your database's username and password
+
+
+{
+    "JWT_SECRET": "", //place your JWT secret string here
+    "CONNECTION_STRING": "", //place the URI of your mongo database here
+    "EMAIL_USER": "", //place the Email address here
+    "EMAIL_PASS":"", //place the App Password here
+}
